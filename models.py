d2beb010723949186e02f9a24e280023a0e5d779
Hannes <hannes.porombka@gmail.com>
2023-03-31 01:03:24 +0200

    updated value length tag name

--- models.py
+++ models.py
@@ -71,7 +71,7 @@ class Tag(db.Model):
     id = db.Column(db.Integer,
                    primary_key = True,
                    autoincrement = True)
-    tag_name = db.Column(db.String(20),
+    tag_name = db.Column(db.Text,
                      nullable = False,
                     unique = True)
     
